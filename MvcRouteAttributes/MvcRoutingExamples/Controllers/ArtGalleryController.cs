af9542ed99347ec3a92857237d6a272c79145a17
gbilyuko <gbilyukov@hhogdev.com>
2014-10-31 17:24:17 +0200

    Created Solution and Project

---
+++ MvcRouteAttributes/MvcRoutingExamples/Controllers/ArtGalleryController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MvcRoutingExamples.Controllers
+{
+    public class ArtGalleryController : Controller
+    {
+        public ActionResult Index()
+        {
+            return new EmptyResult();
+        }
+    }
+}
---
+++ MvcRouteAttributes/MvcRoutingExamples/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcRoutingExamples.MvcApplication" Language="C#" %>
