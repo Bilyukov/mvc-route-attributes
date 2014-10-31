0713b5b07acc40371e828a8a4dd960e54e61241b
gbilyuko <gbilyukov@hhogdev.com>
2014-10-31 17:48:12 +0200

    Created Example Actions

--- MvcRouteAttributes/MvcRoutingExamples/Controllers/ArtGalleryController.cs
+++ MvcRouteAttributes/MvcRoutingExamples/Controllers/ArtGalleryController.cs
@@ -2,9 +2,33 @@
 
 namespace MvcRoutingExamples.Controllers
 {
+    //common prefix for the entire controller
+    // www.foo.com/modern-art
+    [RoutePrefix("modern-art")]
+    //default action
+    // www.foo.com/modern-art points to Index action
+    [Route("{action=index}")]
     public class ArtGalleryController : Controller
     {
         public ActionResult Index()
+        {
+            return new EmptyResult();
+        }
+
+        //artist is an optional parameter
+        //adding ? means that the parameter is not required
+        // www.foo.com/modern-art/paintings
+        // www.foo.com/modern-art/paintings/Auerbach
+        [Route("paintings/{artist?}")]
+        public ActionResult Paintings(string artist)
+        {
+            return new EmptyResult();
+        }
+
+        //tilde (~) override the main route prefix
+        // www.foo.com/gallery/12
+        [Route("~/gallery/{key}")]
+        public ActionResult Gallery(string key)
         {
             return new EmptyResult();
         }
--- MvcRouteAttributes/MvcRoutingExamples/Controllers/HomeController.cs
+++ MvcRouteAttributes/MvcRoutingExamples/Controllers/HomeController.cs
@@ -4,9 +4,12 @@ namespace MvcRoutingExamples.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return new EmptyResult();
         }
+
+
     }
 }
